9021a46512b1ae5b73705523438714ef878b34a1
Marcos Magueta <maguetamarcos@gmail.com>
2023-11-15 21:37:46 -0300

    add xcode

---
+++ Chime/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Chime
+//
+//  Created by Marcos Magueta on 15/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
--- src/main.m
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        NSLog(@"Hello, Chime!");
-    }
-    return 0;
-}
